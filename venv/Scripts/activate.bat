335b42106444e37f8a52fe253e2060f7e79c56ae
duduszchaida <duduszchaida12@gmail.com>
2025-06-24 11:27:32 -0300

    Remover arquivos

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\eduar\OneDrive\Pictures\front\reembolso\venv
+set VIRTUAL_ENV=C:\Users\eduar\OneDrive\Pictures\front\reembolso-inml\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
